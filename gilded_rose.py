b6368923b8a66f28bea277d3c12b91d628cceea7
John <johngerace@gmail.com>
2024-11-05 14:33:02 -0500

    Remove comment

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,38 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-"""
-Proposed changes
-
-* "Conjured" seems like the kind of property that should be set on an item
-    - Each item should store whether it's conjured or not
-* Concept of "Legendary" should be similar to Conjured
-* "Aging" as in brie could be similar, with different quality adjustment rules
-* "Backstage passes" or "Concert ticket" could be its own item
-* Create new item classes: ConjuredItem, LegendaryItem, AgedItem, TicketItem
-    - Inherit from Item
-* Might be nice if each type of item subclass held logic for updating quality
-    - Upper limit stored on each item class
-* Update quality should iterate through each item in the inventory and call the corresponding
-  update_quality() method
-* If we're not allowed to touch Item class, we can create NormalItem class
-
-In thinking about domain boundaries, I think it makes sense for the GildedRose class,
-as an inventory system, to decide *when* to update quality, and for each item to know
-about *how* to update quality. Quality and how it changes is a property of an item,
-not a property of a store.
-
-The other nice thing about refactoring the update_quality logic into each sub class is that
-we now have the ability to decouple from the "name" property. We could conceivably have
-multiple types of each item and they could have different names.
-
-The drawback to doing it this way, as we can see from the texttest fixture is that we now
-have to change the way we instantiate these items. We can't just use the Item class we
-started with. This, to me, is a small price to pay for what I see as cleaner organization,
-increased flexibility, and a proper separation of concerns.
-"""
-
-
 class GildedRose(object):
 
     def __init__(self, items):
